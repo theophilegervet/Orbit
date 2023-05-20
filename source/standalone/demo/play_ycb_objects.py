6c68a0e857aaa133f129a7fefd087de1aae0a065
Theo Gervet <theop123@meta.com>
2023-05-20 13:39:36 -0400

    play around

--- source/standalone/demo/play_ycb_objects.py
+++ source/standalone/demo/play_ycb_objects.py
@@ -124,11 +124,11 @@ def main():
     stage = omni.usd.get_context().get_stage()
     for prim in stage.TraverseAll():
         if prim.GetTypeName() == "Mesh":
-            print(prim.GetName(), prim.GetPrimPath())
+            print(prim.GetName(), prim.GetPrimPath(), prim.GetAttribute('purpose').Get())
 
     print("Get reference to one...")
     prim = stage.GetPrimAtPath("/World/Objects/crackerBox2")
-    print(prim.GetName(), prim.GetPrimPath())
+    print(prim.GetName(), prim.GetPrimPath(), prim.GetAttribute('purpose').Get())
 
     # ------------------------------------------------------------------------------------
 
